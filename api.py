edb49484015af854dcec4944049db6ea1472c05c
Joaquin Vasquez <joaquin.vasquez@live.com>
2021-03-12 11:00:20 -0600

    My first commit

--- api.py
+++ api.py
@@ -7,14 +7,15 @@ ind="index.html"
 @app.route('/my_app', methods=['GET','POST'])
 def my_app():
     if request.method != 'GET':
-        print("not GET")
+        #print("not GET")
+        
         return render_template(ind)
     else:
         req_data = requests.get("https://rss.itunes.apple.com/api/v1/us/podcasts/top-podcasts/all/100/explicit.json")
         data = req_data.json()
         resul = data['feed']['results']
         name = request.args.get('name_1')
-        return render_template(ind, params=resul, name=name)
+        return render_template(ind, params=resul)
 
 @app.route("/"+ind, methods=['GET','POST'])
 def index():
